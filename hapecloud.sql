1c4b349f1e61cf2e78e5657113101baa3683cc79
10779164 <10779164@qq.com>
2018-10-30 00:49:37 -0400

    update

---
+++ hapecloud.sql
@@ -0,0 +1,10 @@
+#mysql -uroot -p < hapecloud.sql
+###create database
+create database Hapecloud charset utf8;
+
+###create database user
+grant all privileges on Hapecloud.* to hapecloud@'%' identified by 'hapecloudadmin';
+
+###create user table
+create table user(id smallint(6) auto_increment unique key,user varchar(10) primary key,password varchar(20) not null,level smallint(6),register_time varchar(20),last_login varchar(20));
+
