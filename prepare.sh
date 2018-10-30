20d3e96236c2189b052a9f3f8b73341533de71f5
10779164 <10779164@qq.com>
2018-10-30 00:49:03 -0400

    add init shell

--- prepare.sh
+++ prepare.sh
@@ -8,12 +8,16 @@ NC='\033[0m'
 . /etc/os-release
 
 if echo $ID | grep -iq "centos"; then
-	yum install epel-release python-pip -y &>/dev/zero
+	yum install epel-release python-pip MySQL-python mariadb-server -y
 	mkdir -p /data/flask_data               
 	echo -e "${green}done${NC}"
+elif echo $ID | grep -iq "debian"; then
+	apt-get install libmysqlclient-dev python-dev python-pip MySQL-python mysql-server -y
+	mkdir -p /data/flask_data
+        echo -e "${green}done${NC}"
 else
-	echo -e "${red}不想写太多系统版本的判断，请使用centos的系统(内核版本>=3.10)${NC}"
-	exit 0
+	echo -e "${red}不想写太多系统版本的判断，请使用centos/ubuntu的系统${NC}"
+        exit 0
 fi
 
 
