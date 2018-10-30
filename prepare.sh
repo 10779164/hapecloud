0e8e0bc5eefd375719de6acb2f1ffaab29c96eff
10779164 <10779164@qq.com>
2018-10-30 04:10:06 -0500

    update

--- prepare.sh
+++ prepare.sh
@@ -11,10 +11,15 @@ if echo $ID | grep -iq "centos"; then
 	yum install epel-release python-pip -y &>/dev/zero
 	mkdir -p /data/flask_data               
 	echo -e "${green}done${NC}"
+elif echo $ID | grep -iq "debian"; then
+	
+
 else
 	echo -e "${red}不想写太多系统版本的判断，请使用centos的系统(内核版本>=3.10)${NC}"
-	exit 0
+	#exit 0
+	apt install python-pip -y
+	mkdir -p /data/flask_data
+        echo -e "${green}done${NC}"
 fi
 
 
-~
