5278425c0dc66abbe7fab23fa97a17d5720133f4
tony <10779164@qq.com>
2019-04-19 17:26:28 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 #bash font color
 red='\033[0;31m'
@@ -8,18 +8,26 @@ NC='\033[0m'
 . /etc/os-release
 
 if echo $ID | grep -iq "centos"; then
-	yum install epel-release python-pip MySQL-python mariadb-server -y
-	mkdir -p /data/flask_data               
-	echo -e "${green}done${NC}"
-	
-elif echo $ID | grep -iq "debian"; then
-	apt-get install libmysqlclient-dev python-dev python-pip MySQL-python mysql-server -y
-	mkdir -p /data/flask_data
+        yum install epel-release python-pip MySQL-python mariadb-server -y
+        mkdir -p /data/flask_data
         echo -e "${green}done${NC}"
-	
+
+elif echo $ID | grep -iq "debian"; then
+        apt-get install libmysqlclient-dev python-dev python-pip mysql-server -y
+        pip install MySQL-python
+        mkdir -p /data/flask_data
+        echo -e "${green}done${NC}"
+
+elif echo $ID | grep -iq "ubuntu"; then
+        apt-get install libmysqlclient-dev python-dev python-pip mysql-server -y
+        pip install MySQL-python
+        mkdir -p /data/flask_data
+        echo -e "${green}done${NC}"
+
 else
-	echo -e "${red}不想写太多系统版本的判断，请使用centos/ubuntu的系统${NC}"
+        echo -e "${red}不想写太多系统版本的判断，请使用centos/ubuntu的系统${NC}"
         exit 0
 fi
+
 
 
